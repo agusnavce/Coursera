897bfc409888528c25c980dcd6c735d99444cc8b
Agustin <agus-navce52@hotmail.com>
2017-08-07 13:13:16 -0700

    update del ejercicio 4 terminado

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -115,7 +115,8 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
-
+        Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m)*(Theta1(:,2:end));
+        Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m)*(Theta2(:,2:end));
 
 
 
